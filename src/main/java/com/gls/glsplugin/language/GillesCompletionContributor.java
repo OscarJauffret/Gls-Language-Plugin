f85f4e097c92471277018a2d067eba076d876550
JauffretOscar <oscar.jauffret@ulb.be>
2024-11-20 20:58:05 +0100

    Added ifel template

--- src/main/java/com/gls/glsplugin/language/GillesCompletionContributor.java
+++ src/main/java/com/gls/glsplugin/language/GillesCompletionContributor.java
@@ -1,20 +1,13 @@
 package com.gls.glsplugin.language;
 
-import com.gls.glsplugin.language.psi.GillesAssignBlock;
-import com.gls.glsplugin.language.psi.GillesTokenType;
-import com.gls.glsplugin.language.psi.GillesTypes;
 import com.gls.glsplugin.language.psi.impl.GillesCodeImpl;
 import com.intellij.codeInsight.completion.*;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.patterns.PlatformPatterns;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiErrorElement;
-import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -24,29 +17,6 @@ public class GillesCompletionContributor extends CompletionContributor {
     private final Map<String, Set<String>> firstSets = new HashMap<>();
 
     public GillesCompletionContributor() {
-        initFirstSets();
-        extend(CompletionType.BASIC,
-                PlatformPatterns.psiElement(),
-                new CompletionProvider<>() {
-                    @Override
-                    protected void addCompletions(@NotNull CompletionParameters parameters,
-                                                  @NotNull ProcessingContext context,
-                                                  @NotNull CompletionResultSet resultSet) {
-                        String keyword = parameters.getPosition().getText();
-                        String regex = "IntellijIdeaRulezzz";
-                        // remove the IntellijIdeaRulezzz suffix
-                        keyword = keyword.replaceAll(regex, "");
-                        System.out.println("keyword: " + keyword);
-                        if (keyword.toLowerCase().startsWith("if")) {
-                            resultSet.addElement(LookupElementBuilder.create("IF {} THEN\n    \nEND:")
-                                    .withInsertHandler((context1, item) -> {
-                                        int startOffset = context1.getEditor().getCaretModel().getOffset();
-                                        context1.getEditor().getCaretModel().moveToOffset(startOffset - 16);
-                                    }));
-                        }
-                    }
-                });
-
         extend(CompletionType.BASIC,
                 PlatformPatterns.psiElement()
                         .inside(PlatformPatterns.psiElement(GillesCodeImpl.class)),
@@ -55,88 +25,12 @@ public class GillesCompletionContributor extends CompletionContributor {
                     protected void addCompletions(@NotNull CompletionParameters parameters,
                                                   @NotNull ProcessingContext context,
                                                   @NotNull CompletionResultSet resultSet) {
-                        // Suggestions pour les instructions dans Code
-                        Set<String> instructions = Set.of("IF", "WHILE", "OUT", "IN", "VARNAME");
+                        // Add possible instructions
+                        Set<String> instructions = Set.of("IF", "WHILE", "OUT", "FOR", "IN", "VARNAME");
                         for (String instruction : instructions) {
                             resultSet.addElement(LookupElementBuilder.create(instruction));
                         }
                     }
                 });
-
-//        extend(CompletionType.BASIC, PlatformPatterns.psiElement(), new CompletionProvider<>() {
-//            @Override
-//            protected void addCompletions(@NotNull CompletionParameters parameters,
-//                                          @NotNull ProcessingContext context,
-//                                          @NotNull CompletionResultSet resultSet) {
-//                PsiElement position = parameters.getPosition();
-//                String contextKey = getContextKey(position);
-//                if (contextKey == null) {
-//                    return;
-//                }
-//                Set<String> possibleContexts = parseContext(contextKey);
-//                System.out.println("possibleContexts: " + possibleContexts);
-//                for (String possibleContext : possibleContexts) {
-//                    Set<String> suggestions = firstSets.get(possibleContext);
-//                    if (suggestions != null) {
-//                        for (String suggestion : suggestions) {
-//                            resultSet.addElement(LookupElementBuilder.create(suggestion));
-//                        }
-//                    } else {
-//                        if (possibleContext.startsWith("GillesTokenType.")) {
-//                            resultSet.addElement(LookupElementBuilder.create(possibleContext.substring(16)));
-//                        }
-//                    }
-//                }
-//            }
-//        });
-    }
-    private void initFirstSets() {
-        firstSets.put("<program>", Set.of("LET"));
-        firstSets.put("<code>", Set.of("IF", "WHILE", "OUT", "IN", "VARNAME", "END", "ELSE"));
-        firstSets.put("<instruction>", Set.of("IF", "WHILE", "OUT", "IN", "VARNAME"));
-        firstSets.put("<assign>", Set.of("VARNAME"));
-        firstSets.put("<expr arith>", Set.of( "-", "VARNAME", "NUMBER", "("));
-        firstSets.put("<expr arith tail>", Set.of("+", "-", ":", ")", "==", "<=", "<", "->", "|", "}"));
-        firstSets.put("<prod>", Set.of( "-", "VARNAME", "NUMBER", "("));
-        firstSets.put("<prod tail>", Set.of("*", "/", "+", "-", ":", ")", "==", "<=", "<", "->", "|", "}"));
-        firstSets.put("<factor>", Set.of( "-", "VARNAME", "NUMBER", "("));
-        firstSets.put("<atom>", Set.of("VARNAME", "NUMBER", "("));
-        firstSets.put("<if>", Set.of("IF"));
-        firstSets.put("<if tail>", Set.of("ELSE", "END"));
-        firstSets.put("<cond>", Set.of( "|", "-", "VARNAME", "NUMBER", "("));
-        firstSets.put("<cond tail>", Set.of("->", "}", "|"));
-        firstSets.put("<final cond>", Set.of( "|", "-", "VARNAME", "NUMBER", "("));
-        firstSets.put("<comp>", Set.of("<", "<=", "=="));
-        firstSets.put("<while>", Set.of("WHILE"));
-        firstSets.put("<output>", Set.of("OUT"));
-        firstSets.put("<input>", Set.of("IN"));
-    }
-
-    private String getContextKey(PsiElement position) {
-        PsiElement parent = position.getParent();
-        while (parent != null && parent.getNode() != null) {
-            if (parent instanceof PsiErrorElement) {
-                String errorMessage = ((PsiErrorElement) parent).getErrorDescription();
-                if (errorMessage.contains("expected")) {
-                    return errorMessage.substring(0, errorMessage.indexOf("expected"));
-                }
-            } else {
-                String elementType = parent.getNode().getElementType().toString();
-                if (firstSets.containsKey(elementType)) {
-                    return elementType;
-                }
-            }
-            parent = parent.getParent();
-        }
-        return null;
-    }
-
-    private Set<String> parseContext(String contextKey) {
-        Set<String> possibleContexts = new HashSet<>();
-        String[] elements = contextKey.split(",|\\s+or\\s+");
-        for (String element : elements) {
-            possibleContexts.add(element.trim());
-        }
-        return possibleContexts;
     }
 }
